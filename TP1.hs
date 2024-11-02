2103fc3fad018a6c7c426ff6b5438a55d5724d19
Eduardo Santos <edu.santos04.vc@gmail.com>
2024-11-02 20:21:17 +0000

    organizar algumas cenas

--- TP1.hs
+++ TP1.hs
@@ -1,6 +1,7 @@
 import qualified Data.List
 import qualified Data.Array
 import qualified Data.Bits
+import qualified Data.Array as Array
 
 
 -- PFL 2024/2025 Practical assignment 1
@@ -54,6 +55,63 @@ shortestPath roadMap start end
     | start == end = [[start]]  -- Caso especial onde o ponto inicial é o mesmo que o destino
     | otherwise    = filterMinPaths (bfs [[start]] roadMap end) roadMap
 
+--travelSales :: RoadMap -> Path
+--travelSales = undefined
+
+tspBruteForce :: RoadMap -> Path
+tspBruteForce roadmap =
+    let cityList = cities roadmap
+        startCity = head cityList  -- Use the first city as the starting point
+        -- Generate all permutations of cities except the starting city to ensure round-trip routes
+        allRoutes = [startCity : route ++ [startCity] | route <- Data.List.permutations (tail cityList)]
+        -- Calculate distances for valid routes and filter out any invalid routes (i.e., Nothing distances)
+        validRoutes = [(route, pathDistance roadmap route) | route <- allRoutes]
+        -- Filter to only include routes with valid distances
+        validDistances = [(route, d) | (route, Just d) <- validRoutes]
+    in case validDistances of
+        [] -> []  -- Return an empty path if no valid routes are found
+        _  -> fst $ findMinDistance validDistances
+
+
+-- Some graphs to test your work
+gTest1 :: RoadMap
+gTest1 = [("7","6",1),("8","2",2),("6","5",2),("0","1",4),("2","5",4),("8","6",6),("2","3",7),("7","8",7),("0","7",8),("1","2",8),("3","4",9),("5","4",10),("1","7",11),("3","5",14)]
+
+gTest2 :: RoadMap
+gTest2 = [("0","1",10),("0","2",15),("0","3",20),("1","2",35),("1","3",25),("2","3",30)]
+
+gTest3 :: RoadMap -- unconnected graph
+gTest3 = [("0","1",4),("2","3",2)]
+
+gTest4 :: RoadMap
+gTest4 = [("A", "B", 5), ("A", "C", 10), ("A", "D", 8), ("A", "E", 15),
+          ("B", "C", 7), ("B", "D", 12), ("B", "E", 9),
+          ("C", "D", 4), ("C", "E", 11),
+          ("D", "E", 6)]
+test :: RoadMap -> City -> [City]
+test roadMap = undefined
+
+--type Graph = [(City,[City],Bool)]
+
+-- FUNÇÕES AUXILIARES 
+
+-- Usadas no 7.isStronglyConnected
+
+-- Função auxiliar para encontrar todas as cidades acessíveis a partir de uma cidade inicial
+reachableFrom :: RoadMap -> City -> [City]
+reachableFrom roadMap start = dfs roadMap [start] []
+
+-- Implementação de DFS para percorrer o grafo e encontrar cidades acessíveis
+dfs :: RoadMap -> [City] -> [City] -> [City]
+dfs _ [] visited = visited
+dfs roadMap (c:cs) visited
+    | c `elem` visited = dfs roadMap cs visited  -- Cidade já visitada, continuar
+    | otherwise = dfs roadMap (adjacentCities ++ cs) (c:visited)
+    where
+        adjacentCities = [neighbor | (neighbor, _) <- adjacent roadMap c]
+
+-- Usadas no 8.ShortestPath
+
 -- Função BFS para explorar todos os caminhos possíveis
 bfs :: [Path] -> RoadMap -> City -> [Path]
 bfs [] _ _ = [] -- quando não houver mais caminhos na "fila" retorna 
@@ -75,58 +133,10 @@ filterMinPaths paths roadMap =
               in [path | path <- paths, pathDistance roadMap path == Just minDist]
 
 
-travelSales :: RoadMap -> Path
-travelSales = undefined
-
-tspBruteForce :: RoadMap -> Path
-tspBruteForce roadmap =
-    let cityList = cities roadmap
-        startCity = head cityList  -- Use the first city as the starting point
-        -- Generate all permutations of cities except the starting city to ensure round-trip routes
-        allRoutes = [startCity : route ++ [startCity] | route <- Data.List.permutations (tail cityList)]
-        -- Calculate distances for valid routes and filter out any invalid routes (i.e., Nothing distances)
-        validRoutes = [(route, pathDistance roadmap route) | route <- allRoutes]
-        -- Filter to only include routes with valid distances
-        validDistances = [(route, d) | (route, Just d) <- validRoutes]
-    in case validDistances of
-        [] -> []  -- Return an empty path if no valid routes are found
-        _  -> fst $ findMinDistance validDistances
-
+-- Usada no 10. TspBruteForce
 -- Helper function to find the route with the minimum distance
 findMinDistance :: [(Path, Distance)] -> (Path, Distance)
 findMinDistance (x:xs) = foldl minByDistance x xs
   where
     minByDistance acc@(route1, dist1) current@(route2, dist2) =
         if dist2 < dist1 then current else acc
--- Some graphs to test your work
-gTest1 :: RoadMap
-gTest1 = [("7","6",1),("8","2",2),("6","5",2),("0","1",4),("2","5",4),("8","6",6),("2","3",7),("7","8",7),("0","7",8),("1","2",8),("3","4",9),("5","4",10),("1","7",11),("3","5",14)]
-
-gTest2 :: RoadMap
-gTest2 = [("0","1",10),("0","2",15),("0","3",20),("1","2",35),("1","3",25),("2","3",30)]
-
-gTest3 :: RoadMap -- unconnected graph
-gTest3 = [("0","1",4),("2","3",2)]
-
-gTest4 :: RoadMap
-gTest4 = [("A", "B", 5), ("A", "C", 10), ("A", "D", 8), ("A", "E", 15),
-          ("B", "C", 7), ("B", "D", 12), ("B", "E", 9),
-          ("C", "D", 4), ("C", "E", 11),
-          ("D", "E", 6)]
-test :: RoadMap -> City -> [City]
-test roadMap = undefined
-
---type Graph = [(City,[City],Bool)]
-
--- Função auxiliar para encontrar todas as cidades acessíveis a partir de uma cidade inicial
-reachableFrom :: RoadMap -> City -> [City]
-reachableFrom roadMap start = dfs roadMap [start] []
-
--- Implementação de DFS para percorrer o grafo e encontrar cidades acessíveis
-dfs :: RoadMap -> [City] -> [City] -> [City]
-dfs _ [] visited = visited
-dfs roadMap (c:cs) visited
-    | c `elem` visited = dfs roadMap cs visited  -- Cidade já visitada, continuar
-    | otherwise = dfs roadMap (adjacentCities ++ cs) (c:visited)
-    where
-        adjacentCities = [neighbor | (neighbor, _) <- adjacent roadMap c]
